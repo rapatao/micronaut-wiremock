89528b5d5ff2dc180992cbccdf94998839ddfc60
Luiz H. Rapatão <rapatao@rapatao.com>
2022-09-08 19:53:03 -0300

    release: 1.0.1 (#2)
    Bump dependencies

--- src/main/kotlin/com/rapatao/micronaut/wiremock/Configuration.kt
+++ src/main/kotlin/com/rapatao/micronaut/wiremock/Configuration.kt
@@ -3,10 +3,21 @@ package com.rapatao.micronaut.wiremock
 import io.micronaut.context.annotation.ConfigurationProperties
 import io.micronaut.context.annotation.Requires
 
+/**
+ * Mapping of WireMock configuration
+ */
 @Requires(property = "wiremock.enabled", value = "true", defaultValue = "true")
 @ConfigurationProperties("wiremock")
 class Configuration {
+
+    /**
+     * Server port
+     */
     @SuppressWarnings("MagicNumber")
     var port: Int = 9090
+
+    /**
+     * Server binding address
+     */
     var host: String = "localhost"
 }
